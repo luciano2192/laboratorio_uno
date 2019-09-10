bce6b13deb95d3f168c9f0af2af27bbc9b8c7418
alumno <lucianogreco92@gmail.com>
2019-09-09 22:04:33 -0300

    clase 3

---
+++ clase_3/ejercicio_3/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i;
+    char nombres[15];
+    char apellido[11];
+
+    printf("Nombres: ");
+    fgets( nombre , 15 , stdin );
+
+    printf("Apellido: ");
+    fgets( apellido , 11 , stdin );
+
+    strlwr( nombres );
+    strlwr( apellido );
+    for( i = 0 ; i < 15 ; i++ ) {
+
+    }
+
+    return 0;
+}
