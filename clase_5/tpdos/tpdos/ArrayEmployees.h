1f188f77715bae9ef8cf8bc2d07eeb1991206dec
Luciano <lucianogreco92@gmail.com>
2019-10-07 15:18:28 -0300

    Update ArrayEmployees.h

--- clase_5/tpdos/tpdos/ArrayEmployees.h
+++ clase_5/tpdos/tpdos/ArrayEmployees.h
@@ -7,97 +7,132 @@ typedef struct {
     int isEmpty;
 } eEmpleado;
 
-/** \brief To indicate that all position in the array are empty,
- * this function put the flag (isEmpty) in TRUE in all
- * position of the array
- * \param list Employee* Pointer to array of employees
- * \param len int Array length
- * \return int Return (-1) if Error [Invalid length or NULL pointer] - (0) if Ok
- *
+/** \brief Inicializa el array como vacio
+ * \param list eEmpleado* puntero al array empleados
+ * \param len int largo del array
+ * \return int retorna (-1) si hay un error - (0) si NO hay error
  */
 int initEmployees( eEmpleado* list , int len );
 
-/** \brief find an Employee by Id en returns the index position in array.
-*
-* \param list Employee*
+/** \brief busca un empleado por id.
+* \param list eEmpleado*
 * \param len int
 * \param id int
-* \return Return employee index position or (-1) if [Invalid length or NULL
-pointer received or employee not found]
-*
+* \return retorna posicion del empleado - si hay un error retorna (-1)
 */
-
 int findEmployeeById( eEmpleado* list , int len , int id );
 
 
-/** \brief print the content of employees array
+/** \brief imprime la lista de empleados
 *
-* \param list Employee*
+* \param list eEmpleado*
 * \param length int
-* \return int
+* \return lista de empleados cargados
 *
 */
 int printEmployees( eEmpleado* list , int length );
 
-/** \brief add in a existing list of employees the values received as parameters
-* in the first empty position
-* \param list employee*
+/** \brief agrega un empleado a la lista
+* \param list eEmpleado*
 * \param len int
-* \param id int
 * \param name[] char
 * \param lastName[] char
 * \param salary float
 * \param sector int
-* \return int Return (-1) if Error [Invalid length or NULL pointer or without
-free space] - (0) if Ok
+* \return int retorna (-1) si hay un error - (0) si NO hay error
 *
 */
-
 int addEmployee( eEmpleado* list , int len , char name[] , char
 lastName[] , float salary , int sector );
 
-/** \brief Remove a Employee by Id (put isEmpty Flag in 1)
+/** \brief elimina un empleado
 *
-* \param list Employee*
+* \param list eEmpleado*
 * \param len int
 * \param id int
-* \return int Return (-1) if Error [Invalid length or NULL pointer or if can't
-find a employee] - (0) if Ok
+* \return int retorna (-1) si hay un error - (0) si NO hay error
 *
 */
 int removeEmployee( eEmpleado* list , int len , int id );
 
-/** \brief Sort the elements in the array of employees, the argument order
-indicate UP or DOWN order
+/** \brief ordena por sector y apellido
 *
-* \param list Employee*
+* \param list eEmpleado*
 * \param len int
-* \param order int [1] indicate UP - [0] indicate DOWN
-* \return int Return (-1) if Error [Invalid length or NULL pointer] - (0) if Ok
+* \return int retorna (0) al finalizar
 *
 */
 int sortEmployees( eEmpleado* list , int len );
 
+/** \brief verifica si hay lugar en la lista
+*
+* \param list eEmpleado*
+* \param length int
+* \return int retorna (-1) si hay error - si NO hay error el indice libre.
+*
+*/
 int hayLugar( eEmpleado* list , int length );
+
 
 void mostrarEmpleado( eEmpleado* list , int cantidad , int id );
 
+/** \brief pone la primer letra en mayuscula, el resto en minuscula
+*
+* \param text char*
+* \return 
+*
+*/
 void formatear( char* text );
 
+/** \brief aumenta automaticamente el id de los empleados
+*
+* \param list eEmpleado*
+* \param cantidad int
+* \return int retona el id a asignar.
+*
+*/
 int aumentar( eEmpleado* list , int cantidad );
 
-/* Funcion usada para verificar el ingreso de caracteres.Chequea para que
-solo sean admitidas las letras S o N.
-Se ingresa void y devuelve un entero sin signo
-0 si se ingreso la N
-1 si se ingreso la S
+/** \brief verifica que se responda S o N
+*
+* \param msj char*
+* \param msjError char*
+* \return el caracter obtenido
+*
 */
 char verificaSioNo( char* msj , char* msjError );
 
+/** \brief verifica que la lista este vacia
+*
+* \param list eEmpleado*
+* \param len int
+* \param id int
+* \return 1 si esta vacio - 0 si NO esta vacio.
+*
+*/
 int estaVacio( eEmpleado* list , int length );
 
+/** \brief imprime un menu
+*
+*
+*/
 int menuModificacion();
 
+/** \brief verifica que la lista este vacia
+*
+* \param list eEmpleado*
+* \param len int
+* \param id int
+* return -1 si no se modifico nada - 0 si se medifico exitosamente.
+*
+*/
 int modifyEmployee( eEmpleado* list , int len , int id );
 
+/** \brief calcula promedio y total de salarios
+*
+* \param list eEmpleado*
+* \param len int
+* return -1 si hay un error - 0 si NO hay error.
+*
+*/
 int promedioSalarios( eEmpleado* list , int len );
