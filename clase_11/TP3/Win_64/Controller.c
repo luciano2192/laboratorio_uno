962a6f28326e5aa765d3426e9f5a33fd440889a7
alumno <lucianogreco92@gmail.com>
2019-11-04 22:04:15 -0300

    clase

--- clase_11/TP3/Win_64/Controller.c
+++ clase_11/TP3/Win_64/Controller.c
@@ -2,8 +2,7 @@
 #include <stdlib.h>
 #include "LinkedList.h"
 #include "Employee.h"
-#include "./utn.h"
-
+#include "utn.h"
 
 /** \brief Carga los datos de los empleados desde el archivo data.csv (modo texto).
  *
@@ -100,8 +99,8 @@ int controller_ListEmployee(LinkedList* pArrayListEmployee)
     hacer for, dentro llget
     declaro una estructura para guardar los datos
     los getter y despues el printf
-    return 1;
     */
+    return 1;
 }
 
 /** \brief Ordenar empleados
@@ -140,22 +139,22 @@ int controller_saveAsBinary(char* path , LinkedList* pArrayListEmployee)
     return 1;
 }
 
-void menu() {
+int menu() {
 
-    int opcion = 0;
+    int opcion;
 
     system("cls");
-    printf("::::::::::MENU::::::::::");
-    printf("1. Cargar los datos de los empleados desde el archivo data.csv (modo texto).");
-    printf("2. Cargar los datos de los empleados desde el archivo data.csv (modo binario).");
-    printf("3. Alta de empleado");
-    printf("4. Modificar datos de empleado");
-    printf("5. Baja de empleado");
-    printf("6. Listar empleados");
-    printf("7. Ordenar empleados");
-    printf("8. Guardar los datos de los empleados en el archivo data.csv (modo texto).");
-    printf("9. Guardar los datos de los empleados en el archivo data.csv (modo binario).");
-    printf("10. Salir");
+    printf("\n::::::::::::::::::::::::::::::MENU::::::::::::::::::::::::::::::\n");
+    printf("\n1. Cargar los datos de los empleados desde el archivo data.csv (modo texto).");
+    printf("\n2. Cargar los datos de los empleados desde el archivo data.csv (modo binario).");
+    printf("\n3. Alta de empleado");
+    printf("\n4. Modificar datos de empleado");
+    printf("\n5. Baja de empleado");
+    printf("\n6. Listar empleados");
+    printf("\n7. Ordenar empleados");
+    printf("\n8. Guardar los datos de los empleados en el archivo data.csv (modo texto).");
+    printf("\n9. Guardar los datos de los empleados en el archivo data.csv (modo binario).");
+    printf("\n10. Salir");
     fflush(stdin);
 
     opcion = getInt( "Seleccionar opcion entre <1-10>: " , "Error ! valor incorrecto." , 1 , 10 );
